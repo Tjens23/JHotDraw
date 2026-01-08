96acbfb3023e9adf4112b4ad02b1fa8c677f6e38
tjens23 <tjens23@student.sdu.dk>
2026-01-08 10:38:51 +0100

    Comments

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/io/ImageFormatProvider.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/io/ImageFormatProvider.java
@@ -4,46 +4,15 @@ import org.jhotdraw.draw.figure.ImageHolderFigure;
 
 public interface ImageFormatProvider {
     
-    /**
-     * Returns the format name (e.g., "PNG", "JPEG").
-     * 
-     * @return the format name
-     */
     String getFormatName();
     
-    /**
-     * Returns a human-readable description of the format.
-     * 
-     * @return the format description
-     */
     String getDescription();
     
-    /**
-     * Returns the file extensions supported by this format.
-     * 
-     * @return array of file extensions (without dots)
-     */
     String[] getFileExtensions();
     
-    /**
-     * Returns the MIME types supported by this format.
-     * 
-     * @return array of MIME types
-     */
     String[] getMimeTypes();
     
-    /**
-     * Creates an input format for reading images of this type.
-     * 
-     * @param prototype the prototype figure for holding the image
-     * @return an InputFormat instance
-     */
     InputFormat createInputFormat(ImageHolderFigure prototype);
     
-    /**
-     * Creates an output format for writing images of this type.
-     * 
-     * @return an OutputFormat instance, or null if output is not supported
-     */
     OutputFormat createOutputFormat();
 }
--- jhotdraw-core/src/main/java/org/jhotdraw/draw/io/PngFormatProvider.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/io/PngFormatProvider.java
@@ -1,21 +1,8 @@
-/*
- * @(#)PngFormatProvider.java
- *
- * Copyright (c) 2025 The authors and contributors of JHotDraw.
- * You may not use, copy or modify this file, except in compliance with the
- * accompanying license terms.
- */
 package org.jhotdraw.draw.io;
 
 import org.jhotdraw.draw.figure.ImageHolderFigure;
 import java.awt.image.BufferedImage;
 
-/**
- * Provider for PNG (Portable Network Graphics) image format.
- * 
- * <p>PNG supports lossless compression and transparency (alpha channel),
- * making it ideal for graphics with sharp edges or text.</p>
- */
 public class PngFormatProvider implements ImageFormatProvider {
     
     @Override
